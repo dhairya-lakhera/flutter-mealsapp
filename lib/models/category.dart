112af5a1996b074376111f9c8989119f74e09bb5
dhairyaCoder <dhairya.coder@gmail.com>
2021-12-27 17:19:03 +0530

    162-163 Navigation to new page and passing data to new page

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 class Category {
   final String id;
   final String title;
-  //For the Color need to import material.dart
   final Color color;
 
   const Category({
